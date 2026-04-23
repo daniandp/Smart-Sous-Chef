1c20b47cb47ec8e31d3613d49704549b176370ba
Justin Hunt <c0951111@mylambton.ca>
2026-04-22 23:36:08 -0400

    Add spoonacular apibut not fully integerated

--- api.py
+++ api.py
@@ -4,3 +4,26 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+API_KEY = os.getenv("SPOONACULAR_API_KEY")
+
+def get_recipes(ingredients):
+    try: 
+        findByIngUrl = "https://api.spoonacular.com/recipes/findByIngredients"
+
+        params ={
+            "ingredients": ingredients,
+            "number": 10,
+            "apiKey": API_KEY
+
+        }
+
+        response = requests.get(findByIngUrl, params=params)
+
+        if response.status_code != 200:
+            
+            return None
+        
+        return response.json()
+    except Exception as err:
+        print("API Error")
+        return None
